09ab2c21ed5069e804cc8568cc4773cd75fda1ea
birdman7260 <mbird7260@gmail.com>
2015-05-16 18:07:12 -0400

    New git ignore

---
+++ app/src/androidTest/java/com/lucu/buddy/buddysystem/BuddyApplicationTest.java
@@ -0,0 +1,13 @@
+package com.lucu.buddy.buddysystem;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class BuddyApplicationTest extends ApplicationTestCase<Application> {
+    public BuddyApplicationTest() {
+        super(Application.class);
+    }
+}
